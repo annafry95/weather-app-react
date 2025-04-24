63db1878429bab0835f0fc81653dfd8f8849766f
annafry95 <annafry95@gmail.com>
2025-04-24 10:36:48 +0100

    Added bootstrap, general layout and CSS

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/anna-fry-a75446198/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Anna Fry
-        </a>{" "}
-        and is opened-sourced on{" "}
-        <a
-          href="https://github.com/annafry95/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/anna-fry-a75446198/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Anna Fry
+          </a>{" "}
+          and is opened-sourced on{" "}
+          <a
+            href="https://github.com/annafry95/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
